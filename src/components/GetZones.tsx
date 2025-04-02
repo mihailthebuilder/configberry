1370be9b3ee9c787f69647b99050d81ac0a2b7ac
mmarian <mihailmarian12@gmail.com>
2025-04-02 14:40:31 +0100

    getzones inputs

--- src/components/GetZones.tsx
+++ src/components/GetZones.tsx
@@ -1,5 +1,18 @@
 function GetZones() {
-  return <div>Zones</div>;
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <input
+        type="text"
+        placeholder="Enter API Key"
+        className="border border-gray-400 rounded px-4 py-2 w-200"
+      />
+      <input
+        type="email"
+        placeholder="Enter Email"
+        className="border border-gray-400 rounded px-4 py-2 w-200"
+      />
+    </div>
+  );
 }
 
 export default GetZones;
