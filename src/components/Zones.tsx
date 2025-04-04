790a9d00256ec7452a7e75ab6331a12a7da6fb6a
mmarian <mihailmarian12@gmail.com>
2025-04-04 21:15:16 +0100

    final checklist

--- src/components/Zones.tsx
+++ src/components/Zones.tsx
@@ -185,55 +185,57 @@ function Zones() {
       )}
 
       {zones.length > 0 && (
-        <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 rounded-lg">
-          <table className="min-w-full divide-y divide-gray-300">
-            <thead className="bg-gray-50">
-              <tr>
-                <th></th>
-                <th
-                  scope="col"
-                  className="py-3.5 pr-3 text-left text-sm font-semibold text-gray-900"
-                >
-                  Name
-                </th>
-                <th
-                  scope="col"
-                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                >
-                  Zone ID
-                </th>
-              </tr>
-            </thead>
-            <tbody className="divide-y divide-gray-200 bg-white">
-              {zones.map((zone) => (
-                <tr key={zone.id}>
-                  <td>
-                    <input
-                      type="checkbox"
-                      className="w-6 h-6 mt-2 ml-2 text-blue-600 focus:ring-blue-500 border-gray-300 rounded hover:cursor-pointer"
-                      onChange={() => {
-                        if (selectedZones.includes(zone)) {
-                          setSelectedZones((prev) =>
-                            prev.filter((z) => z.id !== zone.id)
-                          );
-                        } else {
-                          setSelectedZones((prev) => [...prev, zone]);
-                        }
-                      }}
-                      checked={selectedZones.includes(zone)}
-                    />
-                  </td>
-                  <td className="whitespace-nowrap py-2 pr-3 text-sm font-medium text-gray-900">
-                    {zone.name}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-2 text-sm text-gray-500 font-mono">
-                    {zone.id}
-                  </td>
+        <>
+          <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 rounded-lg">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th></th>
+                  <th
+                    scope="col"
+                    className="py-3.5 pr-3 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Zone ID
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody className="divide-y divide-gray-200 bg-white">
+                {zones.map((zone) => (
+                  <tr key={zone.id}>
+                    <td>
+                      <input
+                        type="checkbox"
+                        className="w-6 h-6 mt-2 ml-2 text-blue-600 focus:ring-blue-500 border-gray-300 rounded hover:cursor-pointer"
+                        onChange={() => {
+                          if (selectedZones.includes(zone)) {
+                            setSelectedZones((prev) =>
+                              prev.filter((z) => z.id !== zone.id)
+                            );
+                          } else {
+                            setSelectedZones((prev) => [...prev, zone]);
+                          }
+                        }}
+                        checked={selectedZones.includes(zone)}
+                      />
+                    </td>
+                    <td className="whitespace-nowrap py-2 pr-3 text-sm font-medium text-gray-900">
+                      {zone.name}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-2 text-sm text-gray-500 font-mono">
+                      {zone.id}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </>
       )}
 
       {zones.length > 0 && (
