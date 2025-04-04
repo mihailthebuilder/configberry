7cfa8bf568866af17c69b89ce08cb8e540113b99
mmarian <mihailmarian12@gmail.com>
2025-04-04 20:06:29 +0100

    only 1 page

--- src/components/Zones.tsx
+++ src/components/Zones.tsx
@@ -44,10 +44,6 @@ function Zones() {
 
   return (
     <div className="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-lg">
-      <a href="/" className="text-blue underline font-bold">
-        Home
-      </a>
-
       <div className="mb-8">
         <h1 className="text-2xl font-bold text-gray-800 mb-4">
           Cloudflare Zones
