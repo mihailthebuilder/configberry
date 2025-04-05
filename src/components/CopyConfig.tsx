ffb46bea747f69222548aa3b1348e2df6f913b5f
mmarian <mihailmarian12@gmail.com>
2025-04-05 19:48:11 +0100

    start of copy config

---
+++ src/components/CopyConfig.tsx
@@ -0,0 +1,10 @@
+function CopyConfig() {
+  return (
+    <div>
+      <h2>Copy Configuration</h2>
+      <p>Use this feature to copy your configuration settings.</p>
+    </div>
+  );
+}
+
+export default CopyConfig;
