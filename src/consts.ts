f04e1c9afbbcaf0d0f3b86aa7f7dd08f8658e667
mmarian <mihailmarian12@gmail.com>
2025-04-17 20:48:23 +0100

    meta tags on landing page

---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE =
+  "ConfigBerry | Manage Cloudflare rules across multiple domains";
+export const SITE_DESCRIPTION =
+  "Collection of tools to manage Cloudflare rules a large number of multiple zones and accounts. Bulk export, import, copy, and delete rules.";
+export const SITE_URL = "https://configberry.com";
+export const SITE_SOCIAL_IMAGE = "./meta.webp";
