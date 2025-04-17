a666f93ae2da8cd935a5eb1f709de372e66162f7
mmarian <mihailmarian12@gmail.com>
2025-04-17 21:06:55 +0100

    export zone tool

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE =
-  "ConfigBerry | Manage Cloudflare rules across multiple domains";
+  "ConfigBerry | Manage Cloudflare Rules Across Multiple Domains";
 export const SITE_DESCRIPTION =
   "Collection of tools to manage Cloudflare rules a large number of multiple zones and accounts. Bulk export, import, copy, and delete rules.";
 export const SITE_URL = "https://configberry.com";
