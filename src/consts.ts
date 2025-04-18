0b151e0234578ebe34be8edf5b927884cc3fd43a
mmarian <mihailmarian12@gmail.com>
2025-04-18 07:57:44 +0100

    meta image inline

--- src/consts.ts
+++ src/consts.ts
@@ -6,4 +6,3 @@ export const SITE_TITLE =
 export const SITE_DESCRIPTION =
   "Collection of tools to manage Cloudflare rules a large number of multiple zones and accounts. Bulk export, import, copy, and delete rules.";
 export const SITE_URL = "https://configberry.com";
-export const SITE_SOCIAL_IMAGE = "./meta.webp";
