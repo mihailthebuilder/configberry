70e91902b9960228623ee4effbb14d669d5944f2
Mihail Marian <mihailthebuilder@gmail.com>
2025-12-30 04:44:59 +0000

    switch back to other paas

--- src/lib/cf.ts
+++ src/lib/cf.ts
@@ -2,7 +2,7 @@ import Cloudflare from "cloudflare";
 
 export function cloudflareClient(apiEmail: string, apiKey: string) {
   return new Cloudflare({
-    baseURL: "https://app.configberry.com/client/v4",
+    baseURL: "https://api.configberry.com/client/v4",
     apiEmail: apiEmail,
     apiKey: apiKey,
   });
